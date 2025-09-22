12f8e011db1314cd14f9f35f41300a0cec6d3456
eyerusalem <4u.aplus.tutor@gmail.com>
2025-09-22 12:02:12 +0300

    change to match case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -3,16 +3,17 @@ number2 = int(input("Enter the second number: "))
 
 operation = input("Choose the operation (+, -, *, /): ")
 
-if operation == "+":
-    print( number1 + number2)
-elif operation == "-":
-    print(number1 - number2)
-elif operation == "*":
-    print(number1 * number2)
-elif operation == "/":
-    if number2 == 0:
-        print("Cannot divide by zero.")
-    else:
-        print(number1 / number2)
-else:
-    print("please enter a number")
+match operation:
+    case "+":
+        print(number1 + number2)
+    case "-":
+        print(number1 - number2)
+    case "*":
+        print(number1 * number2)
+    case "/":
+        if number2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            print(number1 / number2)
+    case _:
+        print("Please enter a valid operation.")
